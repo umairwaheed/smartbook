8dab310b1967b27fd09000830e1d24b9c6fee399
Umair Khan <umair.waheed@gmail.com>
2025-02-26 09:46:28 +0500

    add models

---
+++ books/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Book(models.Model):
+    """Model to store Project Gutenberg book details."""
+    gutenberg_id = models.PositiveIntegerField(unique=True)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, blank=True, null=True)
+    language = models.CharField(max_length=50, blank=True, null=True)
+    download_url = models.URLField()
+    text = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}" if self.author else self.title
+
+
+class UserBookAccess(models.Model):
+    """Model to track books accessed by users."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    accessed_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'book')
+
+    def __str__(self):
+        return f"{self.user.username} accessed {self.book.title}"
