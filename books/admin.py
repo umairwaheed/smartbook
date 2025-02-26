7bcb0e9aead2ead516f95268b610835996607ab8
Umair Khan <umair.waheed@gmail.com>
2025-02-27 02:07:38 +0500

    add admin

---
+++ books/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from .models import Book, UserBookAccess
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "author", "language", "gutenberg_id", "created_at")
+    search_fields = ("title", "author", "gutenberg_id")
+    list_filter = ("language", "created_at")
+    ordering = ("id",)
+
+
+class UserBookAccessAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "book", "accessed_at")
+    search_fields = ("user__username", "book__title")
+    list_filter = ("accessed_at",)
+    ordering = ("-accessed_at",)
+
+
+# Register models
+admin.site.register(Book, BookAdmin)
+admin.site.register(UserBookAccess, UserBookAccessAdmin)
---
+++ users/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    )
+    search_fields = ("username", "email")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+    ordering = ("id",)
+
+
+admin.site.register(User, CustomUserAdmin)
