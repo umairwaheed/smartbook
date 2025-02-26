da11f3c1a56df2cec8f38712bdcdc26042369384
Umair Khan <umair.waheed@gmail.com>
2025-02-27 03:21:15 +0500

    integrate prettier

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        emptyOutDir: true,
-        manifest: true,
+  plugins: [react()],
+  build: {
+    emptyOutDir: true,
+    manifest: true,
+  },
+  server: {
+    proxy: {
+      "/api": "http://127.0.0.1:8000", // Proxy API requests to Django
     },
-    server: {
-        proxy: {
-            '/api': 'http://127.0.0.1:8000',  // Proxy API requests to Django
-        }
-    }
+  },
 });
