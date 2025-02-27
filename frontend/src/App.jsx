6ecc9993d0d916a924671b0531691437ad980f5a
Umair Khan <umair.waheed@gmail.com>
2025-02-27 20:58:22 +0500

    Add userbooks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route, Link } from "react-router";
 import Logout from "./components/Logout";
 import Books from "./pages/Books";
+import UserBooks from "./pages/UserBooks";
 
-const Home = () => <h2 className="text-xl font-bold">🏠 Home Page</h2>;
 const Search = () => <h2 className="text-xl font-bold">🔍 Search Page</h2>;
 
 export default function App() {
@@ -12,7 +12,7 @@ export default function App() {
       <aside className={`bg-gray-800 text-white w-64 p-4 transition-all md:block`}>
         <ul>
           <li className="py-2 hover:bg-gray-700 p-2 rounded">
-            <Link to="/">Home</Link>
+            <Link to="/">My Books</Link>
           </li>
           <li className="py-2 hover:bg-gray-700 p-2 rounded">
             <Link to="/search">Search</Link>
@@ -34,7 +34,7 @@ export default function App() {
         {/* Main Content */}
         <main className="flex-1 p-6 bg-white rounded-lg shadow-md m-4">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<UserBooks />} />
             <Route path="/search" element={<Search />} />
             <Route path="/books" element={<Books />} />
           </Routes>
