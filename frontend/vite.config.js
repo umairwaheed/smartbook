dc28b3262847eb9aafcc908f219b8c9cbacea31a
Umair Khan <umair.waheed@gmail.com>
2025-02-27 14:16:21 +0500

    integrate tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 
 // Change the base to where your static files will be served from
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   base: "/static/",
   server: {
     proxy: {
