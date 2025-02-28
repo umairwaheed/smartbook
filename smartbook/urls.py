a56f2b5add098c5672352b35889dfb664100670e
Umair Khan <umair.waheed@gmail.com>
2025-02-28 05:07:55 +0500

    fix reload issue

--- smartbook/urls.py
+++ smartbook/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 from frontend.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index, name="index"),
     path("users/", include("users.urls")),
     path("api/", include("books.urls")),
+    re_path(r"^(?:.*)/?$", index, name="index"),
 ]
