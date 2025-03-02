2be19f751888ba61b7373ddf4fd17549f7262dc1
Umair Khan <umair.waheed@gmail.com>
2025-03-03 02:21:31 +0500

    code cleanup

--- users/views.py
+++ users/views.py
@@ -13,7 +13,7 @@ def login_view(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect("index")  # Redirect to home page or dashboard
+                return redirect("index")
             else:
                 form.add_error(None, "Invalid username or password.")
     else:
