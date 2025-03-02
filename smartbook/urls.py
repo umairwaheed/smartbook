19aad92cdf882923078d00fc5e3830165abae567
Umair Khan <umair.waheed@gmail.com>
2025-03-02 08:33:29 +0500

    add url for favicon

--- smartbook/urls.py
+++ smartbook/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.views.generic import RedirectView
 
 from frontend.views import index
 
@@ -7,5 +8,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("api/", include("books.urls")),
+    path("favicon.ico", RedirectView.as_view(url="/static/favicon.ico", permanent=True)),
     re_path(r"^(?:.*)/?$", index, name="index"),
 ]
